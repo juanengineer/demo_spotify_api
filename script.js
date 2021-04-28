0d855f325fb20449feecbbe65984971183de3849
juanengineer <82479357+juanengineer@users.noreply.github.com>
2021-04-28 14:50:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ $( document ).ready(function(){
         for (i=0; i< sURLVariables.length; i++){
             sParameterName = sURLVariables[i].split('=');
             if (sParameterName[0]=== sParam){
-                return sParameterName[0] === undefined ? true : decodeURIComponent(sParameterfName[1]);
+                return sParameterName[0] === undefined ? true : decodeURIComponent(sParameterName[1]);
             }
         }
     };
